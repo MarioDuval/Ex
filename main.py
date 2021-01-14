2ea6cd1fe736e43ee682e6e3db85cafaea4cecd9
MarioDuval <77450981+MarioDuval@users.noreply.github.com>
2021-01-14 16:39:19 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from math import sin
+def main():
+    '''
+    word = "python"
+    print(word[2])
+    print(word[-1])
+    print(len(word))
+    print(word[-6])
+    print(word[0:2])
+    print(word[2:])
+    print(word[2:-1])
+    print(word[-2:])
+    print(word[:99])
+    print(word[99:0])
+    print(word[::-1])
+    word = 'N' +word[1:]
+    print(word)
+    '''
+    a = sin(45)
+
+if __name__ == '__main__':
+    main()
